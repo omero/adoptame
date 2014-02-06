2003abd8a784c27c17ff5283df595de812452bdf
Manuel Betancourt Garza <manuelbg@gmail.com>
2014-02-06 12:12:11 -0600

    Base
    Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adoptame::Application.config.secret_key_base = '8fb17746267545bd6b47a3b0dc8d949be490ea5e4c2db2e5441dc88bb9310cfbeaa99cfd14ea4607b47f35b19a9e3197f2a0e297166e52a9a8c0094f55ec2507'
