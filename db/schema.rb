761c661acdfdae45f508353ad42d446b4c516f0f
Manuel Betancourt Garza <manuel@gmail.com>
2014-04-02 02:00:25 -0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140402075659) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.integer  "company_id"
+    t.string   "username"
+    t.string   "name"
+    t.string   "surname"
+    t.string   "gender"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
