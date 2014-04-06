ae79adda02905a5b72ef391b32647cbb6ebce676
Manuel Betancourt Garza <manuel@gmail.com>
2014-04-06 03:26:02 -0500

    commit garbage 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   before_action :authenticate!, only: [:dashboard]
   def home
   end
+
+  def dashboard
+  	case 
+    when admin_signed_in?
+      render "admin_dashboard", layout: "admin"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,23 @@
 module ApplicationHelper
-	def close_session_path
-		case 
-		when admin_signed_in?
-			destroy_admin_session_path
-		when user_signed_in?
-			destroy_user_session_path
-		end
-	end
+  def close_session_path
+    case
+    when admin_signed_in?
+      destroy_admin_session_path
+    when user_signed_in?
+      destroy_user_session_path
+    end
+  end
 
-	def active?
-		
-	end
+  def active?
+    
+  end
+
+  def menu_for_account_type
+    case
+    when admin_signed_in?
+      render "layouts/menu_for_admins"
+    else
+
+    end
+  end
 end
